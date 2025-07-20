3054f22ec2dfc4db27ba43ae39a0a9ce1ef09137
sivertheisholt <sivert.b.heisholt@protonmail.com>
2025-07-20 16:36:50 +0200

    feat: language query param due to default not working in api

--- wondyframe/src/api/warframe_client.rs
+++ wondyframe/src/api/warframe_client.rs
@@ -1,8 +1,11 @@
+use std::collections::HashMap;
+
 use reqwest::Client;
 use serde::de::DeserializeOwned;
 
 pub struct WarframeClient {
     base_url: String,
+    query_params: HashMap<String, String>,
     client: Client,
 }
 
@@ -10,6 +13,7 @@ impl WarframeClient {
     pub fn new(platform: &str) -> Self {
         Self {
             base_url: format!("https://api.warframestat.us/{}/", platform),
+            query_params: HashMap::from([("language".to_string(), "en".to_string())]),
             client: Client::new(),
         }
     }
@@ -23,7 +27,14 @@ impl WarframeClient {
         } else {
             format!("{}{}", self.base_url, endpoint)
         };
-        let response = self.client.get(&url).send().await?.json::<T>().await?;
+        let response = self
+            .client
+            .get(&url)
+            .query(&self.query_params)
+            .send()
+            .await?
+            .json::<T>()
+            .await?;
         Ok(response)
     }
 }
